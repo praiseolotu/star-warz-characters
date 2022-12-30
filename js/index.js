d895af0a80a9389b8203b23213df3b1747c45cbb
JEDIBASIL <jedidiahbasil@gmail.com>
2022-12-30 20:04:47 +0100

    fetched the api

--- js/index.js
+++ js/index.js
@@ -1,9 +1,55 @@
+const cardContainer = document.querySelector(".card_container");
+const searchInput = document.querySelector(".search-box")
 
-fetch("https://swapi.dev/api/people", {
-    method: 'GET',
-    headers: {
-        'Content-Type': "application / json"
+let starWarzData = [];
+let filteredStarWarzData = []
+
+const cardUILoader = () => {
+    for (let i = 1; i < 9; i++) {
+        cardContainer.innerHTML += `
+    <div class="loader_card" >
+      <div class="image"></div>
+      <div class="details_container">
+        <p class="text"></p>
+    </div> `;
     }
-})
-    .then(res => res.json())
-    .then(data => console.log(data))
+};
+
+const updateSearch = (value = "") =>{
+    cardContainer.innerHTML =""
+    filteredStarWarzData = starWarzData
+    .filter(data => data.name.toLowerCase().includes(value.toLowerCase().trim()))
+    filteredStarWarzData.map(({name,gender}, i) =>cardUI( name, gender, i ))
+}
+
+
+
+const cardUI = ( name, gender, i ) => {
+    cardContainer.innerHTML += `
+            <div class="card">
+            <img src="https://robohash.org/${name}" />
+            <div class="details_container">
+              <h3>${name}</h3>
+              <p></p>
+            </div>
+        </div>`;
+};
+
+cardUILoader();
+
+
+fetch("https://swapi.dev/api/people", { method: "GET", headers: { "Content-Type": "application / json", }, })
+    .then((res) => res.json())
+    .then((data) => {
+        starWarzData = data.results;
+        updateSearch("")
+        cardContainer.innerHTML =""
+        filteredStarWarzData.map(({name,gender}, i) => 
+        setTimeout(()=>cardUI(name, gender, i),1000))
+    });
+
+    searchInput.addEventListener("input",(e)=>{
+        updateSearch(e.target.value)
+    })
+
+    // import { TfiHome, TfiNotepad, TfiSettings, TfiUnlink, TfiHeart, TfiPowerOff, TfiBlackboard } from "react-icons/tfi"
